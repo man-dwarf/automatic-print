31f76de61f780d6947d25339b89bd5115068ec2d
MGApcDev <mgapcdev@gmail.com>
2020-10-18 20:06:58 +0200

    Observer rename actions for handle download

--- Readme.md
+++ Readme.md
@@ -2,7 +2,6 @@
 - Print a file with Adobe and setup print settings
 - Open powershell
 - Run script
-
 - Close powershell to kill script
 
 
--- print-file.ps1
+++ print-file.ps1
@@ -1,28 +1,30 @@
-# $files = Get-ChildItem "c:\printable\*.pdf"
-
-# foreach ($file in $files){
-#     start-process -FilePath $file.fullName -Verb Print
-#     # Write-Host $file.fullName
-# }
-# start-process -FilePath C:\printable\sample.pdf –Verb Print
-
 
 $filter = '*.pdf'
+# $dirpath = $args[0]
+$dirpath = 'C:\Users\mgapc\Downloads'
 
+$watcher = New-Object System.IO.FileSystemWatcher
+$watcher.Path = $dirpath
+$watcher.Filter = "*.pdf"
+$watcher.EnableRaisingEvents = $true
 
-# $watcher = New-Object System.IO.FileSystemWatcher
-# $watcher.Path = $args[0] 
-# $watcher.EnableRaisingEvents = $true
-
-$watcher = New-Object IO.FileSystemWatcher $args[0], $filter -Property @{IncludeSubdirectories = $false;NotifyFilter = [IO.NotifyFilters]'FileName, LastWrite'}
+# $watcher = New-Object IO.FileSystemWatcher $dirpath, $filter -Property @{IncludeSubdirectories = $false}
 $action =
 {
     $path = $event.SourceEventArgs.FullPath
+    Write-Host $path
+    
+    # $changeType = $Event.SourceEventArgs.ChangeType
+    # Write-Host $changeType
+
     # $ext = [System.IO.Path]::GetExtension($path)
-    # Write-Host $path
     # if ($ext -like '*.pdf') { 
-        start-process -FilePath $path -Verb Print
+    start-process -FilePath $path -Verb Print
     # }
-}
+}   
 
 Register-ObjectEvent $watcher 'Created' -Action $action
+# Downloaded files are renamed from .tmp -> .pdf
+Register-ObjectEvent $watcher 'Renamed' -Action $action
+# Register-ObjectEvent $watcher 'Changed' -Action $action
+# Register-ObjectEvent $watcher 'Deleted' -Action $action
