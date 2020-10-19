621709f39b2292d3fd102ff9cd1b412880d8790a
Mathias Grundtvig Andreasen <man@dwarf.dk>
2020-10-19 16:58:26 +0200

    Update print-file.ps1

--- print-file.ps1
+++ print-file.ps1
@@ -1,26 +1,18 @@
 
-$filter = '*.pdf'
 # $dirpath = $args[0]
-$dirpath = 'C:\Users\mgapc\Downloads'
+$dirpath = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
 
 $watcher = New-Object System.IO.FileSystemWatcher
 $watcher.Path = $dirpath
 $watcher.Filter = "*.pdf"
 $watcher.EnableRaisingEvents = $true
 
-# $watcher = New-Object IO.FileSystemWatcher $dirpath, $filter -Property @{IncludeSubdirectories = $false}
 $action =
 {
     $path = $event.SourceEventArgs.FullPath
     Write-Host $path
     
-    # $changeType = $Event.SourceEventArgs.ChangeType
-    # Write-Host $changeType
-
-    # $ext = [System.IO.Path]::GetExtension($path)
-    # if ($ext -like '*.pdf') { 
     start-process -FilePath $path -Verb Print
-    # }
 }   
 
 Register-ObjectEvent $watcher 'Created' -Action $action
